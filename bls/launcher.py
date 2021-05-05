8111c20b4047d7eb3ec0a11b8b3125675e621716
Stephane WAFFO <guywaffo@gmail.com>
2021-05-05 21:46:57 +0200

    "Correct signing"

--- bls/launcher.py
+++ bls/launcher.py
@@ -1,15 +1,23 @@
+#Script to launch generation sign and verification on gcp
+## The start index is give by  the second computer
+###
+
+
 import subprocess,sys
 import multiprocessing
 NB_CPUS=8
 
-
+##Start is used to parallelized processing
+START=0
+if len (sys.argv)>1:
+    START = int(sys.argv[1])
 
 
 def task(t,n,message_size,id):
     subprocess.call(["python3","gen_sign_verif_gcp.py",str(t),str(n), str(message_size),str(id + 1)])
 
 MESSAGE_SIZES=[64,128]
-N_VALUES=[8]
+N_VALUES=[]
 THRESHOLD_VALUES=[0.5,0.7,0.8,0.9]
 
 
@@ -18,12 +26,13 @@ for message_size in MESSAGE_SIZES:
 
         t_values= list(set( map(lambda ts:int(n*ts)+1,THRESHOLD_VALUES )) )## Unique corresponding t_values
 
+
         for t in t_values :
             if t >n:
                 break
 
             tasks=[]
-            for id in range(n):
+            for id in range(START,START+NB_CPUS):
                 tasks.append(multiprocessing.Process(target=task,args=[t,n,message_size,id]))
 
             for t in tasks:
