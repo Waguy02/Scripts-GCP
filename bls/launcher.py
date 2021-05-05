8875f80f82649e4dfd7d9f338d45c68b2ad9bab5
Stephane WAFFO <guywaffo@gmail.com>
2021-05-05 23:06:07 +0200

    "Correct signing"

--- bls/launcher.py
+++ bls/launcher.py
@@ -25,25 +25,15 @@ THRESHOLD_VALUES=[0.5,0.7,0.8,0.9]
 
 for message_size in MESSAGE_SIZES:
     for n in N_VALUES:
-
-
-
         t_values= list(set( map(lambda ts:int(n*ts)+1,THRESHOLD_VALUES )) )## Unique corresponding t_values
-
-
         for t in t_values :
             if t >n:
                 break
-
             tasks=[]
-
-
             if(START>=n):
                 continue
-
             for id in range(START,START+NB_CPUS):
                 tasks.append(multiprocessing.Process(target=task,args=[t,n,message_size,id]))
-
             for t in tasks:
                 t.start()
             for t in tasks:
