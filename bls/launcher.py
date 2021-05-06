9ce3ae257f9942643028d71b19201829e56d1ab1
Stephane WAFFO <guywaffo@gmail.com>
2021-05-06 07:47:30 +0200

    "Fixing nb parties local exec"

--- bls/launcher.py
+++ bls/launcher.py
@@ -6,8 +6,8 @@
 import subprocess,sys
 import multiprocessing
 import multiprocessing as mp
-
-NB_CPUS=mp.cpu_count()
+import threading
+NB_CPUS=mp.cpu_count()-1
 TASK_PER_CPU=8 ##TASK PER CPU
 
 
@@ -21,13 +21,22 @@ if len (sys.argv)>1:
 
 
 def task(t0,n,message_size,id):
+    subtasks=[]
     for t in range(t0,t0+TASK_PER_CPU):
         if t>n:
             break
-        subprocess.call(["python3","gen_sign_verif_gcp.py",str(t),str(n), str(message_size),str(id + 1)])
-
+        st=threading.Thread(target=lambda :subprocess.call(["python3","gen_sign_verif_gcp.py",str(t),str(n), str(message_size),str(id + 1)]))
+        subtasks.append(st)
+    
+    for st in subtasks:
+        st.start()
+    for st in subtasks:
+        st.join()
+        
+        
+    
 MESSAGE_SIZES=[64,128]
-N_VALUES=[10,20,30,40,49]
+N_VALUES=[10,20,30,50,70,80,90,100,110,120]
 THRESHOLD_VALUES=[0.5,0.7,0.8,0.9]
 
 
