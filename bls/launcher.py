f6df4832a3c1e67ca2426cc44ab76d0cbf08f8b0
Stephane WAFFO <guywaffo@gmail.com>
2021-05-06 22:34:27 +0200

    "Configuration for 15 CPUS"

--- bls/launcher.py
+++ bls/launcher.py
@@ -12,6 +12,7 @@ TASK_PER_CPU=8 ##TASK PER CPU
 
 
 
+
 ##Start is used to parallelized processing
 START=0
 if len (sys.argv)>1:
@@ -24,23 +25,18 @@ def task(t,n,message_size,id):
     subprocess.call(["python3","gen_sign_verif_gcp.py",str(t),str(n), str(message_size),str(id + 1)])
 
 MESSAGE_SIZES=[64,128]
-N_VALUES=[5,8,10,13,15]
+N_VALUES=[5,10,15,50,100,500]
 THRESHOLD_VALUES=[0.3,0.4,0.5,0.7,0.8,0.9]
 
 
 for message_size in MESSAGE_SIZES:
     for n in N_VALUES:
         t_values= list(set( map(lambda ts:int(n*ts)+1,THRESHOLD_VALUES )) )## Unique corresponding t_values
-        for t in t_values :
-            if t >n:
-                break
-            tasks=[]
-            for id in range(n):
-                tasks.append(multiprocessing.Process(target=task,args=[t,n,message_size,id]))
-            for t in tasks:
-                t.start()
-            for t in tasks:
-                t.join()
+        def f(t):
+            return task(t,n,message_size,id)
+        pool = mp.Pool(NB_CPUS, maxtasksperchild=int(NB_CPUS / len(t_values)) + 1)
+        pool.map(f,t_values)
+        pool.join()
 
 
 
